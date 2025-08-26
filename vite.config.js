b7f165f56d29b3007648440441b640b0ef4bee60
m.njoroge <m.njoroge@keshiholdings.com>
2025-08-26 15:13:18 +0300

    added a .env.production

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-export default {
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'axios'],
-        },
-      },
-    },
-  },
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
